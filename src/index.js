0aa59900ee1f5d543bc2179c03876bc8461ce958
ovs1176 <64677914+ovs1176@users.noreply.github.com>
2023-10-07 14:45:42 +0530

    chore: Added necessary comments in index.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// importing css files
 import "./index.css";
+// importing All the necessary component here... 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
